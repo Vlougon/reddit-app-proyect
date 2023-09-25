c85790c9c19060410766b758df558dd11684b61c
Victor <vlougongs@gmial.com>
2023-09-25 18:21:44 +0100

    Actividad 3 Finalizada

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -4,6 +4,7 @@
 
 @foreach ($links as $link)
 <li>{{$link->title}}</li>
+<small>Contributed by: {{$link->creator->name}} {{$link->updated_at->diffForHumans()}}</small>
 @endforeach
 {{$links->links()}}
 
