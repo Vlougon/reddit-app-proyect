c37d20cc8e8fcadd579cb1c82c8e7f38ddb7f57f
Victor <vlougongs@gmial.com>
2023-09-25 17:55:57 +0100

    EJercico 3 Hecho

--- app/Http/Controllers/CommunityLinkController.php
+++ app/Http/Controllers/CommunityLinkController.php
@@ -12,7 +12,8 @@ class CommunityLinkController extends Controller
      */
     public function index()
     {
-        return view('community/index');
+        $links = CommunityLink::paginate(25);
+        return view('community/index', compact('links'));
     }
 
     /**
