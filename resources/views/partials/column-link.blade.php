09e5f3cdcdd79da64f554a70d45b6ccbe38e1e07
Victor <vlougongs@gmail.com>
2023-10-24 12:42:27 +0100

    Actividad 14. Ejercicio 1: Hecho!

--- resources/views/partials/column-link.blade.php
+++ resources/views/partials/column-link.blade.php
@@ -4,11 +4,18 @@
     <a href="{{ $link->link }}" target="_blank">
         {{ $link->title }}
     </a>
-    <small>Contributed by: {{ $link->creator->name }} {{ $link->updated_at->diffForHumans() }}. Votos: {{$link->users()->count()}}</small>
+    <small>Contributed by: {{ $link->creator->name }} {{ $link->updated_at->diffForHumans() }}.</small>
     <span class="label label-default" style="background: {{ $link->channel->color }}">
         <a class="text-decoration-none text-white" href="/community/{{ $link->channel->slug }}">
             {{ $link->channel->title }}
         </a>
     </span>
+
+    <form method="POST" action="/votes/{{ $link->id }}">
+        {{ csrf_field() }}
+        <button type="button" class="btn btn-secondary" {{ Auth::guest() ? 'disabled' : '' }}>
+            {{$link->users()->count()}}
+        </button>
+    </form>
 </li>
 @endforeach
